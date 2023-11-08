1a68020a3a015c2bfdd804185436f3eb13c86ed2
MrBushido21 <olegliskom@gmail.com>
2023-11-08 18:09:16 +0200

    changed server link

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -7,7 +7,7 @@ import EmojiPicker from "emoji-picker-react";
 import s from "../styles/Chat.module.css"
 import { Messages } from "./Messages";
 
-const socket = io.connect('http://localhost:5000')
+const socket = io.connect('https://online-chat-xyxo.onrender.com')
 
 export const Chat = () => {
     const { search } = useLocation()
